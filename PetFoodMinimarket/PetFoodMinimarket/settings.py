18e259bab9b5853c88624b7626c1f1700edbf93f
jimgalleguillos <jimgalleguillos@gmail>
2025-04-02 14:24:44 -0300

    Add admin panel and related apps to project settings

--- PetFoodMinimarket/PetFoodMinimarket/settings.py
+++ PetFoodMinimarket/PetFoodMinimarket/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'admin_panel',
+    'products',
+    'sales',
+    'suppliers',
+    'users',
 ]
 
 MIDDLEWARE = [
